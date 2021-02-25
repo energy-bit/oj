ca41532b66aaac7d9b2b29e020c5fa154c36958a
energy-bit <1037887028@qq.com>
2021-02-25 23:27:54 +0800

    LeetCode  在排序数组中查找数字 I

---
+++ search.cpp
@@ -0,0 +1,54 @@
+﻿/*
+统计一个数字在排序数组中出现的次数。
+
+ 
+
+ 示例 1:
+
+ 输入: nums = [5,7,7,8,8,10], target = 8
+ 输出: 2
+ 示例 2:
+
+ 输入: nums = [5,7,7,8,8,10], target = 6
+ 输出: 0
+
+*/
+class Solution {
+public:
+	int search(vector<int>& nums, int target) {
+		if (nums.size() == 0)
+		{
+			return 0;
+		}
+		int left = 0;
+		int right = nums.size() - 1;
+		while (nums[left] != nums[right])
+		{
+			int mid = (left + right) / 2;
+			if (nums[mid] < target)
+			{
+				left = mid + 1;
+			}
+			else if (nums[mid] > target)
+			{
+				right = mid - 1;
+			}
+			else
+			{
+				if (nums[right] == target)
+				{
+					++left;
+				}
+				else
+				{
+					--right;
+				}
+			}
+		}
+		if (nums[left] == target)
+		{
+			return right - left + 1;
+		}
+		return 0;
+	}
+};
